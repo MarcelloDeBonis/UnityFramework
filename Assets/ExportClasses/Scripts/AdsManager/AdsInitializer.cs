798d4d4698d3d874a167edc1848eb84378376d77
Gabriele Fiorentino <gabrielefiorentino2@gmail.com>
2022-12-15 17:48:52 +0100

    Add AdsManager

---
+++ Assets/ExportClasses/Scripts/AdsManager/AdsInitializer.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+ 
+public class AdsInitializer : Singleton<AdsInitializer>, IUnityAdsInitializationListener
+{
+    [SerializeField] string _androidGameId;
+    [SerializeField] string _iOSGameId;
+    [SerializeField] bool _testMode = true;
+    private string _gameId;
+
+    [SerializeField] private RewardedAdsButton rewardedAdsButton;
+    [SerializeField] private InterstitialAdsButton interstitialAdsButton;
+    void Awake()
+    {
+        InitializeAds();
+    }
+ 
+    public void InitializeAds()
+    {
+        _gameId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOSGameId
+            : _androidGameId;
+        Advertisement.Initialize(_gameId, _testMode, this);
+    }
+ 
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
+        rewardedAdsButton.LoadAd();
+        interstitialAdsButton.LoadAd();
+    }
+ 
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+    }
+}
