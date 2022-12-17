e876f5749235d6a1f6ae99b24766661b55fffe56
marcello de bonis <marcello98.videogames@gmail.com>
2022-12-17 11:29:21 +0100

    InputTouchManager

---
+++ Assets/ExportClasses/Scripts/EnumTouchAction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnumTouchAction
+{
+   NoTouching,
+   Tap,
+   DoubleTap,
+   Drag,
+   Slide,
+   Pinch,
+   Spread,
+   Pressing,
+}
